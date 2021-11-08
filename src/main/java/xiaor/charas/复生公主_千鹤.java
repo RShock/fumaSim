e7a3078687829115a1a61a38ceadd21c6a1b2cc7
咕咕型小r <418648118@qq.com>
2021-11-09 00:14:37 +0800

    feat: 准备露露和千鹤的草稿

--- src/main/java/xiaor/charas/复生公主_千鹤.java
+++ src/main/java/xiaor/charas/复生公主_千鹤.java
@@ -4,25 +4,15 @@ package xiaor.charas;
 import lombok.experimental.SuperBuilder;
 import xiaor.Chara;
 import xiaor.Element;
-import xiaor.GameBoard;
-import xiaor.Tools;
-import xiaor.story.SkillBuilder;
-
-import static xiaor.Common.INFI;
-import static xiaor.TriggerEnum.游戏开始时;
-import static xiaor.TriggerEnum.释放必杀后;
-import static xiaor.story.DamageBuilder.DamageType.必杀伤害;
-import static xiaor.story.DamageBuilder.DamageType.普通伤害;
-import static xiaor.story.SkillType.*;
 
 @SuperBuilder(toBuilder = true)
-public class 法斯公主_露露 extends BaseChara {
+public class 复生公主_千鹤 extends BaseChara {
 
-    public 法斯公主_露露() {
-        this("露露");
+    public 复生公主_千鹤() {
+        this("千鹤");
     }
 
-    public 法斯公主_露露(String name) {
+    public 复生公主_千鹤(String name) {
         super();
         this.name = name;
         this.element = Element.风属性;
@@ -37,11 +27,19 @@ public class 法斯公主_露露 extends BaseChara {
     @Override
     public void initSkills() {
         double[] multi = {0, 0.69, 0.73, 0.76, 0.80, 0.80};
-        //以攻击力200%对我方全体进行治疗，并获得”每回合以攻击力x%进行治疗（5回合）"效果 CD5->4
+        //使目标受到的风属性伤害增加12（2层）  再以攻击力330%对目标造成伤害 CD4
 
-        //以攻击力50%对我方全体进行治疗
+        //普通攻击
 
-        //队长技 使我方全体必杀技伤害增加25%
+        //队长技 使自身攻击力+90% 必杀技伤害增加+30%
+
+        //使自身受到伤害减少15% 但是受到火属性伤害增加30%（不做）
+
+        //防御时，触发：使自身当前必杀技CD减少1回合效果（不做）
+
+        //5星被动 必杀时，触发使我方全体风属性角色必杀技伤害增加15%（最多2层）效果
+
+        //6潜 使得自身必杀技伤害增加10%
 
         //普攻时，触发“以攻击力25%对我方HP最低者进行治疗" hp最低应该是百分比（吃必杀技加成）
 
