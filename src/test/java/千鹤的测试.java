6cb5530382d03836ceb1b295f5332859112f6d66
咕咕型小r <418648118@qq.com>
2021-11-15 21:50:26 +0800

    feat: 再补一个测试

--- src/test/java/千鹤的测试.java
+++ src/test/java/千鹤的测试.java
@@ -86,4 +86,16 @@ class 千鹤的测试 {
         assertEquals(798558, 精灵王.getLife(), 100);
     }
 
+    @Test
+    void 沃沃千鹤联手大战精灵王测试() {
+        胆小纸袋狼_沃沃 沃沃 = 胆小纸袋狼_沃沃.builder().attack(204650).isLeader(true).name("沃沃").skillLevel(2).element(Element.风属性).build();
+        复生公主_千鹤 千鹤 = 复生公主_千鹤.builder().name("千鹤").attack(361418).is6(true).skillLevel(1).element(Element.风属性).skillLevel(1).build();
+        木桩 精灵王 = 木桩.builder().life(11821290).name("精灵王").element(Element.风属性).build();
+        board.addOurChara(沃沃);
+        board.addOurChara(千鹤);
+        board.addEnemyChara(精灵王);
+        board.initSkills();
+        board.run("1a1 2d1 1a1 2d1 1q1 2q1 1a1 2a1 1a1 2d1 1a1 2q1 1a1 2a1");
+        assertEquals(1494377, 精灵王.getLife(), 100);
+    }
 }
